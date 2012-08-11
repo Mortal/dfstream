e34fc2af1868eabc8e20cc89115a0570fd8a6118
Mathias Rav <rav@cs.au.dk>
2012-08-11 13:02:17 +0200

    Add support for Portable Pixel Maps (PPM)

--- compressor.cpp
+++ compressor.cpp
@@ -1,58 +1,70 @@
 #include <cstring>
 #include <cstdio>
-
-unsigned char lineardecode(const char * input) {
-#include "linear_decider.inl"
-	return 255;
-}
+#include <utility>
+#include <array>
 
 const int symbolwidth = 8; // pixel width of single symbol; must be a power of two
 const int symbolheight = 12;
 const int indexoffsetmask = symbolwidth-1;
 
+template <typename pixel>
 struct deinterlacer {
-	const char * offset;
+	const pixel * offset;
 	const int symbolcolumns;
-	inline deinterlacer(const char * offset, const int symbolcolumns)
+	inline deinterlacer(const pixel * offset, const int symbolcolumns)
 		: offset(offset)
 		, symbolcolumns(symbolcolumns)
 	{
 	}
 
-	inline char operator[](const size_t i) const {
-		return offset[7] != offset[(i & ~indexoffsetmask)*symbolcolumns + (i & indexoffsetmask)];
+	inline pixel getcol(const size_t i) const {
+		return offset[(i & ~indexoffsetmask)*symbolcolumns + (i & indexoffsetmask)];
+	}
+
+	inline bool operator[](const size_t i) const {
+		return getcol(7) != getcol(i);
 	}
 };
 
-unsigned char binarydecode(const deinterlacer & input) {
+template <typename pixel>
+std::pair<unsigned char, unsigned char> binarydecode(const deinterlacer<pixel> & input) {
 #include "binary_decider.inl"
 }
 
-inline int translate(const int index, const int symbolcolumns) {
-	return (index & indexoffsetmask)
-		+ symbolwidth*(index/(symbolcolumns*symbolwidth))
-		+ (symbolwidth*symbolheight)*((index % (symbolcolumns*symbolwidth))/symbolwidth);
+template <int pixelsize>
+bool readpixels(const int width, const int height);
+
+bool readframe() {
+	int format, width, height, maxval;
+	if (4 != fscanf(stdin, "P%d %d %d %d\n", &format, &width, &height, &maxval)) {
+		if (feof(stdin)) return false;
+		printf("Couldn't read header\n");
+		return false;
+	}
+	if (maxval > 255) {
+		printf("Maxval too large: %d\n", maxval);
+		return false;
+	}
+	if (format == 5) return readpixels<1>(width, height);
+	if (format == 6) return readpixels<3>(width, height);
+	printf("Unknown format %d\n", format);
+	return false;
 }
 
-int readframe() {
-	int format, width, height, what;
-	if (4 != fscanf(stdin, "P%d %d %d %d\n", &format, &width, &height, &what)) {
-		if (feof(stdin)) return 0;
-		printf("Couldn't read header\n");
-		return 0;
-	}
-	if (format != 5) {
-		printf("Unknown format %d\n", format);
-		return 0;
-	}
+template <int pixelsize>
+bool readpixels(const int width, const int height) {
+	typedef std::array<char, pixelsize> pixel;
 	const int symbolcolumns = width/symbolwidth;
 	const int symbolrows = height/symbolheight;
 	const int bufsize = width*symbolheight;
-	char * symbolrow = new char[bufsize];
+	pixel * symbolrow = new pixel[bufsize];
 	for (int y = 0; y < symbolrows; ++y) {
-		fread(symbolrow, sizeof(char), bufsize, stdin);
-		for (char * buf = symbolrow; buf != (symbolrow+width); buf += symbolwidth) {
-			putchar(binarydecode(deinterlacer(buf, symbolcolumns)));
+		fread(symbolrow, sizeof(pixel), bufsize, stdin);
+		for (pixel * buf = symbolrow; buf != (symbolrow+width); buf += symbolwidth) {
+			deinterlacer<pixel> d(buf, symbolcolumns);
+			auto c = binarydecode(d);
+			//pixel fg = d.getcol(c.first);
+			putchar(c.second);
 		}
 	}
 	return 1;
