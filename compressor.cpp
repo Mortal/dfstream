89d19cf4ba1e755ed8e01d595d333c100c7b75d4
Mathias Rav <rav@cs.au.dk>
2012-08-12 19:10:41 +0200

    Add stupid hack

--- compressor.cpp
+++ compressor.cpp
@@ -16,6 +16,21 @@ struct dataequal<T, 0, N> {
 	}
 };
 
+template <>
+struct dataequal<unsigned char, 3, 3> {
+	typedef unsigned char T;
+	static const int I = 3;
+	static const int N = 3;
+
+
+	inline static bool eq(const T * a, const T * b) {
+		static const unsigned char mask[] = {255,255,255,0};
+		static const int * imask = reinterpret_cast<const int *>(mask);
+		return (*reinterpret_cast<const int *>(a) & *imask)
+			== (*reinterpret_cast<const int *>(b) & *imask);
+	}
+};
+
 template <typename T, int I, int N>
 struct dataequal {
 	inline static bool eq(const T * a, const T * b) {
